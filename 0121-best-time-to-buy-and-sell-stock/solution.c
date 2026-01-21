77294fb1fcd22115f7e7d041fc0bf1f56ce34399
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-01-21 18:45:27 +0000

    Sync LeetCode submission Runtime - 2 ms (31.12%), Memory - 15.3 MB (100.00%)

---
+++ 0121-best-time-to-buy-and-sell-stock/solution.c
@@ -0,0 +1,14 @@
+int maxProfit(int* prices, int pricesSize) {
+    int maxProfit = 0;
+    int minPrice = prices[0];
+
+    for(int i=1; i<pricesSize; i++){
+       if(prices[i]<minPrice){
+        minPrice = prices[i];
+       }
+       else if(prices[i]-minPrice>maxProfit){
+         maxProfit = prices[i]-minPrice;
+       }
+    }
+    return maxProfit;
+}
