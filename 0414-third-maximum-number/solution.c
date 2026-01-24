1ae9f83642c2a15d86fbb8a802faf789cb1f7d06
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-01-24 20:25:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (100.00%)

---
+++ 0414-third-maximum-number/solution.c
@@ -0,0 +1,25 @@
+int thirdMax(int* nums, int numsSize) {
+    long long first = LLONG_MIN;
+    long long second = LLONG_MIN;
+    long long third = LLONG_MIN;
+
+    for(int i = 0; i < numsSize; i++) {
+        if(nums[i] == first || nums[i] == second || nums[i] == third)
+            continue;
+
+        if(nums[i] > first) {
+            third = second;
+            second = first;
+            first = nums[i];
+        }
+        else if(nums[i] > second) {
+            third = second;
+            second = nums[i];
+        }
+        else if(nums[i] > third) {
+            third = nums[i];
+        }
+    }
+
+    return (third == LLONG_MIN) ? first : third;
+}
