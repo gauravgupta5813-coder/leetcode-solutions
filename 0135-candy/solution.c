ac7362932bf0b7c8bb128694336fd48a3925394c
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-02-03 17:57:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (100.00%)

---
+++ 0135-candy/solution.c
@@ -0,0 +1,25 @@
+int candy(int* ratings, int ratingsSize) {
+
+    int candies[ratingsSize];
+
+    for(int i=0; i<ratingsSize; i++)
+        candies[i] = 1;
+
+    for(int i=1; i<ratingsSize; i++) {
+        if(ratings[i] > ratings[i-1])
+            candies[i] = candies[i-1] + 1;
+    }
+    
+    for(int i=ratingsSize-2; i>=0; i--) {
+        if(ratings[i] > ratings[i+1])
+            candies[i] = (candies[i] > candies[i+1] + 1) ?
+                          candies[i] : candies[i+1] + 1;
+    }
+
+    int sum = 0;
+    for(int i=0; i<ratingsSize; i++)
+        sum += candies[i];
+
+    return sum;
+}
+
