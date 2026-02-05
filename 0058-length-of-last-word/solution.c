d68fe6c87989515bc82582d1b3100a6de630272f
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-02-05 17:50:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (100.00%)

---
+++ 0058-length-of-last-word/solution.c
@@ -0,0 +1,23 @@
+#include<string.h>
+int lengthOfLastWord(char *s) {
+    int len = 0;
+    int i = 0;
+
+    while (s[i] != '\0') {
+        i++;
+    }
+    i--;
+
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    while (i >= 0 && s[i] != ' ') {
+        len++;
+        i--;
+    }
+
+    return len;
+}
+
+
