79143348abdea5a9f4b3920233ed2f9dbb73ab89
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-02-10 05:01:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (100.00%)

---
+++ 4058-compute-alternating-sum/solution.c
@@ -0,0 +1,9 @@
+int alternatingSum(int* nums, int numsSize) {
+    int sum=0;
+    for(int i=0; i<numsSize; i++){
+        if(i%2==0){
+            sum+=nums[i];
+        } else sum-=nums[i];
+    }
+    return sum;
+}
