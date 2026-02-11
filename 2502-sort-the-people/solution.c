d4e6a993b66e8b9201b39c45ff10e26b2961dff7
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-02-11 16:17:25 +0000

    Sync LeetCode submission Runtime - 23 ms (38.04%), Memory - 13.8 MB (100.00%)

---
+++ 2502-sort-the-people/solution.c
@@ -0,0 +1,22 @@
+char** sortPeople(char** names, int namesSize, int* heights, int heightsSize, int* returnSize) {
+
+    for(int i = 0; i < heightsSize - 1; i++) {
+        for(int j = 0; j < heightsSize - i - 1; j++) {
+
+            if(heights[j] < heights[j+1]) {
+
+                int tempH = heights[j];
+                heights[j] = heights[j+1];
+                heights[j+1] = tempH;
+
+                char* tempN = names[j];
+                names[j] = names[j+1];
+                names[j+1] = tempN;
+            }
+        }
+    }
+
+    *returnSize = namesSize;
+    return names;
+}
+
