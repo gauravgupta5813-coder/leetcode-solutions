df35444b8cf05003f1fc6174e926f93249305c2e
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-03-30 17:11:06 +0000

    Sync LeetCode submission Runtime - 3 ms (35.02%), Memory - 13.6 MB (18.86%)

---
+++ 0002-add-two-numbers/solution.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    dummy.next = NULL;
+
+    struct ListNode* temp = &dummy;
+    int carry = 0;
+
+    while(l1 != NULL || l2 != NULL || carry != 0){
+        int sum = carry;
+
+        if(l1 != NULL){
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if(l2 != NULL){
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = sum % 10;
+        newNode->next = NULL;
+
+        carry = sum / 10;
+
+        temp->next = newNode;
+        temp = temp->next;
+    }
+
+    return dummy.next;
+}
