067c0a0f3e36583cb378c0686554e1abd7292fd6
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-04-15 18:07:13 +0000

    Sync LeetCode submission Runtime - 1 ms (33.81%), Memory - 11.6 MB (74.95%)

---
+++ 0496-next-greater-element-i/solution.c
@@ -0,0 +1,28 @@
+int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) 
+{
+    int *ans = (int*) malloc(nums1Size * sizeof(int));
+    *returnSize = nums1Size;
+
+    for(int i = 0; i < nums1Size; i++)
+    {
+        int found = 0;
+        ans[i] = -1;
+
+        for(int j = 0; j < nums2Size; j++)
+        {
+            if(nums1[i] == nums2[j])
+            {
+                for(int k = j + 1; k < nums2Size; k++)
+                {
+                    if(nums2[k] > nums2[j])
+                    {
+                        ans[i] = nums2[k];
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    return ans;
+}
