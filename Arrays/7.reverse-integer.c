58a396a3cf66806821eca90fb77f2525730c8caf
gauravgupta5813-coder <gauravgupta5813@gmail.com>
2026-04-30 17:31:50 +0530

    Adding two sum problem

---
+++ Arrays/7.reverse-integer.c
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=7 lang=c
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+int reverse(int x){
+
+}
+// @lc code=end
+
---
+++ Arrays/9.palindrome-number.c
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=9 lang=c
+ *
+ * [9] Palindrome Number
+ */
+
+#include <stdbool.h>
+
+// @lc code=start
+bool isPalindrome(int x) {
+    
+}
+// @lc code=end
+
