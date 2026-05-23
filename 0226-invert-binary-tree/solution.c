df62758c5628d11123c93a3212bc4b0f29927f8d
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-05-23 07:19:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (30.08%)

---
+++ 0226-invert-binary-tree/solution.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+void swap(struct TreeNode **a, struct TreeNode **b){
+    struct TreeNode *temp = *a;
+    *a = *b;
+    *b = temp;
+}
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if(root == NULL) return NULL;
+
+    swap(&root->left, &root->right);
+    invertTree(root->left);
+    invertTree(root->right);
+
+    return root;
+}
