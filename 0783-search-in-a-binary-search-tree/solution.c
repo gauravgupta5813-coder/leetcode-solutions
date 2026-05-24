682ec7f05f2c1943fe0635ba3c9b7bfcb35256b6
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-05-24 10:37:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (10.64%)

---
+++ 0783-search-in-a-binary-search-tree/solution.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if(root == NULL) return NULL;
+
+    if(root->val == val) return root;
+
+    else if(val > root->val) {
+        return searchBST(root->right, val);
+    }
+    else {
+        return searchBST(root->left, val);
+    }
+
+}
