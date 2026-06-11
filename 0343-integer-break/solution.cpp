bd50f14b8279713214c8f3c26ec807f5d63bbae9
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-06-11 12:19:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (77.73%)

---
+++ 0343-integer-break/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        if(n==4) return 4;
+        int max = 1;
+        while(n>4){
+            n = n-3;
+            max *= 3;
+        }
+        return max*n;
+    }
+};
