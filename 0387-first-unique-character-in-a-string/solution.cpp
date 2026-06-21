0674aad8eaa7aebf2c92b006031982a9733aa988
GauravGupta5813 <gauravgupta5813@gmail.com>
2026-06-21 09:46:13 +0000

    Sync LeetCode submission Runtime - 18 ms (26.04%), Memory - 14.6 MB (73.28%)

---
+++ 0387-first-unique-character-in-a-string/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        for(int i=0; i<s.size(); i++){
+            int flag = 0;
+            for(int j=0; j<s.size(); j++){
+                if(s[i]==s[j] && i!=j){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
